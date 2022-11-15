5b47ed59c393dc29d52a112c5cafc62aab3c6149
Alex Grsm <alexey.grsm@gmail.com>
2022-11-15 15:47:13 +0200

    search component is finished

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,9 +1,11 @@
 import langDropDown from './components/langdropdown';
 import superMenu from './components/superMenu';
+import search from './components/search';
 
 window.addEventListener('DOMContentLoaded', () => {
   // 'use strict';
 
   langDropDown();
   superMenu();
+  search();
 });
---
+++ src/js/components/search.js
@@ -0,0 +1,44 @@
+const search = () => {
+  const boxSearch = document.querySelector('.search-box'),
+    btnSearch = document.querySelector('.btn-search'),
+    inputSearch = document.querySelector('.input-search'),
+    // closeSearch = document.querySelector('.search-close'),
+    resultSearch = document.querySelector('.search-result');
+
+  const toggleSearch = () => {
+    boxSearch.classList.toggle('active');
+    if (boxSearch.classList.contains('active')) {
+      inputSearch.focus();
+    } else {
+      inputSearch.blur();
+      resultSearch.classList.remove('active');
+    }
+  };
+
+  btnSearch.addEventListener('click', () => {
+    toggleSearch();
+  });
+
+  document.addEventListener('click', e => {
+    let target = e.target;
+    let itsBoxSearch = target == boxSearch || boxSearch.contains(target);
+    let boxSearchIsActive = boxSearch.classList.contains('active');
+
+    if (!itsBoxSearch && boxSearchIsActive) {
+      toggleSearch();
+    }
+  });
+
+  document.addEventListener('keyup', function (e) {
+    if (e.keyCode == 27) {
+      // escape key maps to keycode `27`
+      toggleSearch();
+    }
+  });
+
+  inputSearch.addEventListener('input', function () {
+    inputSearch.value.length > 2 ? resultSearch.classList.add('active') : resultSearch.classList.remove('active');
+  });
+};
+
+export default search;
